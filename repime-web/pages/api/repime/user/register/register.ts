805d5b624a80cdf022c3e2a36378bdb2defc5383
darlosss <broududs@gmail.com>
2023-05-20 15:29:55 -0300

    Testes — api

--- repime-web/pages/api/repime/user/register/register.ts
+++ repime-web/pages/api/repime/user/register/register.ts
@@ -1,11 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from "../../../../../src/app/libs/prisma_db";
+import { json } from 'stream/consumers';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(
-        {}
-    );
+
+    res.status(200).send(req.body)
 
 }
