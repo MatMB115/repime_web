593ed1f61422b7e1c1da2528f069937153b3c246
darlosss <broududs@gmail.com>
2023-05-20 01:04:57 -0300

    Atualização dos Scripts e Test no Prisma — Lembrar de checar as rotas da api

---
+++ repime-web/src/app/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json(
+        {}
+    );
+
+}
