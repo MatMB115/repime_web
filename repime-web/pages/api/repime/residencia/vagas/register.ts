a5b1d316c6b83437c5e13a1a94289316dcdcbeca
Adriano <66391807+adriano-12@users.noreply.github.com>
2023-06-15 00:44:09 -0300

    R e R das vagas, tá indo com uma foto
    É sobre isso.

---
+++ repime-web/pages/api/repime/residencia/vagas/register.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from "../../../../../src/app/libs/prisma_db";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    try {
+         const vaga = await prisma.vaga.create({
+            data: {
+                mensalidade: Number(req.body.vaga.mensalidade),
+                informacoes_adicionais: req.body.vaga.informacoes_adicionais,
+                tb_residencia: {
+                    connect: {
+                        id: Number(req.body.residencia.id)
+                    }
+                }
+            }
+        });
+
+        await prisma.fotoVaga.create({
+            data: {
+                foto: req.body.fotoVaga.foto,
+                tb_vaga: {
+                    connect: {
+                        id: Number(vaga.id)
+                    }
+                }
+            }
+        });
+
+        return res.status(200).json({
+            "repime": {
+                "cod_ret": 0,
+                "msg_ret": "A vaga foi criada com sucesso!"
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            "repime": {
+                "cod_ret": 1,
+                "msg_ret": (error as Error).message
+            }
+        });
+    }
+}
+
+
