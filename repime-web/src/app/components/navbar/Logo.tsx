39da2494a69df2ae0ffe5a64e0570d730694acda
MatMB115 <MatMB115@users.noreply.github.com>
2023-06-22 07:18:47 -0300

    corrigindo responsitividade do logo e icon

--- repime-web/src/app/components/navbar/Logo.tsx
+++ repime-web/src/app/components/navbar/Logo.tsx
@@ -7,44 +7,31 @@ import { useEffect, useState } from "react";
 const Logo = () => {
     const router = useRouter();
 
-    const [isScreenSmall, setIsScreenSmall] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsScreenSmall(window.innerWidth < 768); // Defina o tamanho de tela em que deseja substituir o logo
-        };
-
-        // Adiciona o event listener para controlar o redimensionamento da tela
-        window.addEventListener('resize', handleResize);
-
-        // Remove o event listener ao desmontar o componente
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
     return (
         <>
-        {isScreenSmall ? (
+            <div className="flex flex-row items-center">
             <Image
             onClick={() => router.push('/')}
             alt="Logo"
             className="md:block cursor-pointer"
             height={"50"}
             width={"50"}
-            src={"/images/logomob.png"}
-            />
-        ) : (
-            <Image
-            onClick={() => router.push('/')}
-            alt="Logo"
-            className="hidden md:block cursor-pointer"
-            height={"125"}
-            width={"125"}
             src={"/images/logo.png"}
             />
-        )}
+                <div 
+                    onClick={() => router.push('/')} 
+                    className="
+                        text-xl 
+                        text-bold 
+                        hidden md:block 
+                        cursor-pointer
+                    "
+                >
+                    <span>Repi</span>
+                    <span className="text-repimeblue">ME</span>
+                </div>
+            </div>
         </>
-        
     );
 }
 
