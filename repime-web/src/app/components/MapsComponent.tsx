b34385d4a08557f596ede78c0bc9e1ebc2caf8cb
britomtb <brito@mtb.ind.br>
2023-07-11 19:01:42 -0300

    (Ajuste) Maps

--- repime-web/src/app/components/MapsComponent.tsx
+++ repime-web/src/app/components/MapsComponent.tsx
@@ -25,10 +25,10 @@ const GoogleMapsComponent: React.FC<GoogleMapsProps> = (
     const [map, setMap] = React.useState(null)
     let positions = [];
 
-    const onLoad = React.useCallback(function callback(map) {
+    const onLoad = React.useCallback(function (map) {
         const geocoder = new window.google.maps.Geocoder();
-        getAddress(geocoder, addressPlace, map, positions, false);
-        getAddress(geocoder, addressUniversity, map, positions, true);
+
+        getAddressResidencia(addressPlace, addressUniversity, map, positions, geocoder)
 
     }, [])
 
@@ -36,15 +36,12 @@ const GoogleMapsComponent: React.FC<GoogleMapsProps> = (
         setMap(null)
         positions = []
     }, [])
-    const options = {
-        minZoom: 8,
-        maxZoom: 15,
-    }
+
     return isLoaded ? (
         <GoogleMap
             mapContainerStyle={containerStyle}
             onLoad={onLoad}
-            options={options}
+            zoom={6}
             onUnmount={onUnmount}
         >
             {
@@ -55,21 +52,46 @@ const GoogleMapsComponent: React.FC<GoogleMapsProps> = (
 
 export default GoogleMapsComponent;
 
-function getAddress(geocoder: google.maps.Geocoder, address: string, map: any, positions, center: boolean) {
+function getAddressResidencia(address: string, addressUniversity: string, map: any, positions, geocoder) {
+    console.log(address);
     geocoder.geocode({ address }, (results, status) => {
         if (status === window.google.maps.GeocoderStatus.OK) {
             const { location } = results![0].geometry;
 
             const marker = {
                 map: map,
-                position: results![0].geometry.location
+                position: results![0].geometry.location,
+                icon: "/images/house_icon.png",
             };
             positions.push({ lat: location.lat(), lng: location.lng() });
             new google.maps.Marker(marker);
-            if (!center) return;
-            console.log(positions);
-            const bounds = new window.google.maps.LatLngBounds({ lat: (positions[0].lat + positions[1].lat) / 2, lng: (positions[0].lng + positions[1].lng) / 2 });
-            map.fitBounds(bounds);
+            getAddressUniversidade(addressUniversity, map, positions, geocoder)
         }
     });
 }
+
+function getAddressUniversidade(address: string, map: any, positions: any, geocoder) {
+    console.log(address);
+    geocoder.geocode({ address }, (results, status) => {
+        if (status === window.google.maps.GeocoderStatus.OK) {
+            const { location } = results![0].geometry;
+
+            const marker = {
+                position: results![0].geometry.location,
+                map: map, icon: "/images/university_icon.png"
+            };
+            positions.push({ lat: location.lat(), lng: location.lng() });
+            new google.maps.Marker(marker);
+            centralizeMaps();
+        }
+        function centralizeMaps() {
+            const centerLat = (positions[0].lat + positions[1].lat) / 2;
+            const centerLng = (positions[0].lng + positions[1].lng) / 2;
+            const bounds = new window.google.maps.LatLngBounds({ lat: centerLat, lng: centerLng });
+            map.fitBounds(bounds);
+            map.setZoom(14)
+        }
+    });
+}
+
+
